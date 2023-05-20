ed3bf9462239c29a6a88b67b3f7c76d56c469f86
Jeanette <haltonjeanette@gmail.com>
2023-05-20 11:02:37 -0700

    Dealing with failed merge

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -12,4 +12,63 @@ const { User, Image, Spot, Review, sequelize } = require("../../db/models");
 
 const router = express.Router();
 
+//Delete a Spot Image
+router.delete("/:imageId", restoreUser, requireAuth, async (req, res, next) => {
+  const image = await Image.findByPk(req.params.imageId, {
+    where: {
+      imageableType: "Spot",
+    },
+  });
+  //If the image doesn't exist
+  if (!image) {
+    const err = new Error(`Spot Image couldn't be found`);
+    err.status = 404;
+    return next(err);
+  }
+
+  const spot = await Spot.findByPk(image.imageableId);
+
+  //If not the owner of the spot
+  if (spot.ownerId !== req.user.id) {
+    const err = new Error(`Forbidden`);
+    err.status = 403;
+    return next(err);
+  }
+
+  await image.destroy();
+  res.status(200);
+  res.json({ message: `Successfully deleted` });
+});
+
+//------------------------------------------------------------------------
+
+//Delete a Review Image
+router.delete("/:imageId", restoreUser, requireAuth, async (req, res, next) => {
+  const image = await Image.findByPk(req.params.imageId, {
+    where: {
+      imageableType: "Review",
+    },
+  });
+
+  //Review Image does not exist
+  if (!image) {
+    const err = new Error(`Review Image couldn't be found`);
+    err.status = 404;
+    return next(err);
+  }
+
+  //Review does not belong to user
+  const review = await Review.findByPk(image.imageableId);
+  if (review.userId !== req.user.id) {
+    const err = new Error(`Forbidden`);
+    err.status = 403;
+    return next(err);
+  }
+
+  //Delete the review image
+  await image.destroy();
+  res.status(200);
+  res.json({ message: `Successfully deleted` });
+});
+
 module.exports = router;
