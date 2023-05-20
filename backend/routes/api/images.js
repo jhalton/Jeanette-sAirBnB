5acd776928628c6defe6799cb1f5854cb8a0f7fe
Jeanette <haltonjeanette@gmail.com>
2023-05-20 10:12:12 -0700

    delete-spot-image-

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -8,8 +8,38 @@ const {
   requireAuth,
   restoreUser,
 } = require("../../utils/auth");
-const { User, Image, Spot, sequelize } = require("../../db/models");
+const { User, Image, Spot, Review, sequelize } = require("../../db/models");
 
 const router = express.Router();
+
+//Delete a Spot Image
+router.delete("/:imageId", restoreUser, requireAuth, async (req, res, next) => {
+  const image = await Image.findByPk(req.params.imageId, {
+    where: {
+      imageableType: "Spot",
+    },
+  });
+  //If the image doesn't exist
+  if (!image) {
+    const err = new Error(`Spot Image couldn't be found`);
+    err.status = 404;
+    return next(err);
+  }
+
+  const spot = await Spot.findByPk(image.imageableId);
+
+  //If not the owner of the spot
+  if (spot.ownerId !== req.user.id) {
+    const err = new Error(`Forbidden`);
+    err.status = 403;
+    return next(err);
+  }
+
+  await image.destroy();
+  res.status(200);
+  res.json({ message: `Successfully deleted` });
+});
+
+//------------------------------------------------------------------------
 
 module.exports = router;
