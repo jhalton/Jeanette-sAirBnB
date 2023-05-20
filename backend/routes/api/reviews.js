366a72540588ab0227a06614ac45635d39549ce1
Jeanette <haltonjeanette@gmail.com>
2023-05-19 20:14:03 -0700

    edit-a-review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -3,7 +3,11 @@ const bcrypt = require("bcryptjs");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser,
+} = require("../../utils/auth");
 const { User, Spot, Review, sequelize } = require("../../db/models");
 
 const router = express.Router();
@@ -17,5 +21,34 @@ const validateReview = [
     .withMessage(`Stars must be an integer from 1 to 5`),
   handleValidationErrors,
 ];
+
+//Edit a Review
+//
+router.put(
+  "/:reviewId",
+  restoreUser,
+  requireAuth,
+  validateReview,
+  async (req, res, next) => {
+    const foundReview = await Review.findByPk(req.params.reviewId);
+    if (!foundReview) {
+      res.status(404);
+      res.json({ message: `Review couldn't be found` });
+      return;
+    }
+    const { review, stars } = req.body;
+    if (foundReview.userId === req.user.id) {
+      const updatedReview = await foundReview.update({
+        review,
+        stars,
+      });
+      res.status(200);
+      res.json(updatedReview);
+    } else {
+      res.status(403);
+      res.json({ message: "Forbidden" });
+    }
+  }
+);
 
 module.exports = router;
