19bc9e44044db80a72b8e316750c751e51169b06
Jeanette <haltonjeanette@gmail.com>
2023-07-14 23:20:47 -0700

    removed test tooltip

--- frontend/src/components/Spots/UpdateSpot.js
+++ frontend/src/components/Spots/UpdateSpot.js
@@ -27,9 +27,9 @@ const UpdateSpot = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { spotId } = useParams();
-  const spot = useSelector((state) => state.singleSpot[spotId]);
+  const spot = useSelector((state) => state.singleSpot[+spotId]);
 
-  console.log("LOOK HERE UPDATE SPOT.ID", spot);
+  console.log("LOOK HERE UPDATE SPOT", spot);
 
   const [country, setCountry] = useState(spot.country);
   const [address, setAddress] = useState(spot.address);
